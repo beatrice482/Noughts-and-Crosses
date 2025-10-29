3717fe398ff0e8037d69864f422199c462b88573
Beatrice <beasmith06@icloud.com>
2025-10-29 14:11:12 +0000

    improvements and class added

--- noughts-and-crosses/Form1.Designer.cs
+++ noughts-and-crosses/Form1.Designer.cs
@@ -29,147 +29,48 @@ namespace noughts_and_crosses
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_1 = new System.Windows.Forms.Button();
-            this.btn_2 = new System.Windows.Forms.Button();
-            this.btn_3 = new System.Windows.Forms.Button();
-            this.btn_4 = new System.Windows.Forms.Button();
-            this.btn_5 = new System.Windows.Forms.Button();
-            this.btn_6 = new System.Windows.Forms.Button();
-            this.btn_7 = new System.Windows.Forms.Button();
-            this.btn_8 = new System.Windows.Forms.Button();
-            this.btn_9 = new System.Windows.Forms.Button();
-            this.lbl_game = new System.Windows.Forms.Label();
-            this.btn_reset = new System.Windows.Forms.Button();
             this.lbl_title = new System.Windows.Forms.Label();
+            this.btn_AI = new System.Windows.Forms.Button();
+            this.btn_2Player = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btn_1
-            // 
-            this.btn_1.Location = new System.Drawing.Point(204, 78);
-            this.btn_1.Name = "btn_1";
-            this.btn_1.Size = new System.Drawing.Size(111, 105);
-            this.btn_1.TabIndex = 0;
-            this.btn_1.UseVisualStyleBackColor = true;
-            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
-            // 
-            // btn_2
-            // 
-            this.btn_2.Location = new System.Drawing.Point(321, 78);
-            this.btn_2.Name = "btn_2";
-            this.btn_2.Size = new System.Drawing.Size(111, 105);
-            this.btn_2.TabIndex = 1;
-            this.btn_2.UseVisualStyleBackColor = true;
-            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
-            // 
-            // btn_3
-            // 
-            this.btn_3.Location = new System.Drawing.Point(438, 78);
-            this.btn_3.Name = "btn_3";
-            this.btn_3.Size = new System.Drawing.Size(111, 105);
-            this.btn_3.TabIndex = 2;
-            this.btn_3.UseVisualStyleBackColor = true;
-            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
-            // 
-            // btn_4
-            // 
-            this.btn_4.Location = new System.Drawing.Point(204, 189);
-            this.btn_4.Name = "btn_4";
-            this.btn_4.Size = new System.Drawing.Size(111, 105);
-            this.btn_4.TabIndex = 3;
-            this.btn_4.UseVisualStyleBackColor = true;
-            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
-            // 
-            // btn_5
-            // 
-            this.btn_5.Location = new System.Drawing.Point(321, 189);
-            this.btn_5.Name = "btn_5";
-            this.btn_5.Size = new System.Drawing.Size(111, 105);
-            this.btn_5.TabIndex = 4;
-            this.btn_5.UseVisualStyleBackColor = true;
-            this.btn_5.Click += new System.EventHandler(this.btn_5_Click);
-            // 
-            // btn_6
-            // 
-            this.btn_6.Location = new System.Drawing.Point(438, 189);
-            this.btn_6.Name = "btn_6";
-            this.btn_6.Size = new System.Drawing.Size(111, 105);
-            this.btn_6.TabIndex = 5;
-            this.btn_6.UseVisualStyleBackColor = true;
-            this.btn_6.Click += new System.EventHandler(this.btn_6_Click);
-            // 
-            // btn_7
-            // 
-            this.btn_7.Location = new System.Drawing.Point(204, 300);
-            this.btn_7.Name = "btn_7";
-            this.btn_7.Size = new System.Drawing.Size(111, 105);
-            this.btn_7.TabIndex = 6;
-            this.btn_7.UseVisualStyleBackColor = true;
-            this.btn_7.Click += new System.EventHandler(this.btn_7_Click);
-            // 
-            // btn_8
-            // 
-            this.btn_8.Location = new System.Drawing.Point(321, 300);
-            this.btn_8.Name = "btn_8";
-            this.btn_8.Size = new System.Drawing.Size(111, 105);
-            this.btn_8.TabIndex = 7;
-            this.btn_8.UseVisualStyleBackColor = true;
-            this.btn_8.Click += new System.EventHandler(this.btn_8_Click);
-            // 
-            // btn_9
-            // 
-            this.btn_9.Location = new System.Drawing.Point(438, 300);
-            this.btn_9.Name = "btn_9";
-            this.btn_9.Size = new System.Drawing.Size(111, 105);
-            this.btn_9.TabIndex = 8;
-            this.btn_9.UseVisualStyleBackColor = true;
-            this.btn_9.Click += new System.EventHandler(this.btn_9_Click);
-            // 
-            // lbl_game
-            // 
-            this.lbl_game.AutoSize = true;
-            this.lbl_game.Location = new System.Drawing.Point(328, 40);
-            this.lbl_game.Name = "lbl_game";
-            this.lbl_game.Size = new System.Drawing.Size(104, 17);
-            this.lbl_game.TabIndex = 9;
-            this.lbl_game.Text = "Player 1\'s Turn";
-            this.lbl_game.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // btn_reset
-            // 
-            this.btn_reset.Location = new System.Drawing.Point(626, 341);
-            this.btn_reset.Name = "btn_reset";
-            this.btn_reset.Size = new System.Drawing.Size(141, 64);
-            this.btn_reset.TabIndex = 10;
-            this.btn_reset.Text = "Reset";
-            this.btn_reset.UseVisualStyleBackColor = true;
-            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
             // 
             // lbl_title
             // 
             this.lbl_title.AutoSize = true;
-            this.lbl_title.Location = new System.Drawing.Point(304, 9);
+            this.lbl_title.Location = new System.Drawing.Point(325, 81);
             this.lbl_title.Name = "lbl_title";
             this.lbl_title.Size = new System.Drawing.Size(144, 17);
             this.lbl_title.TabIndex = 11;
             this.lbl_title.Text = "Noughts and Crosses";
+            // 
+            // btn_AI
+            // 
+            this.btn_AI.Location = new System.Drawing.Point(451, 186);
+            this.btn_AI.Name = "btn_AI";
+            this.btn_AI.Size = new System.Drawing.Size(141, 72);
+            this.btn_AI.TabIndex = 12;
+            this.btn_AI.Text = "Play Computer";
+            this.btn_AI.UseVisualStyleBackColor = true;
+            this.btn_AI.Click += new System.EventHandler(this.btn_AI_Click);
+            // 
+            // btn_2Player
+            // 
+            this.btn_2Player.Location = new System.Drawing.Point(202, 183);
+            this.btn_2Player.Name = "btn_2Player";
+            this.btn_2Player.Size = new System.Drawing.Size(141, 75);
+            this.btn_2Player.TabIndex = 13;
+            this.btn_2Player.Text = "2 Player";
+            this.btn_2Player.UseVisualStyleBackColor = true;
+            this.btn_2Player.Click += new System.EventHandler(this.btn_2Player_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_2Player);
+            this.Controls.Add(this.btn_AI);
             this.Controls.Add(this.lbl_title);
-            this.Controls.Add(this.btn_reset);
-            this.Controls.Add(this.lbl_game);
-            this.Controls.Add(this.btn_9);
-            this.Controls.Add(this.btn_8);
-            this.Controls.Add(this.btn_7);
-            this.Controls.Add(this.btn_6);
-            this.Controls.Add(this.btn_5);
-            this.Controls.Add(this.btn_4);
-            this.Controls.Add(this.btn_3);
-            this.Controls.Add(this.btn_2);
-            this.Controls.Add(this.btn_1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -179,19 +80,9 @@ namespace noughts_and_crosses
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btn_1;
-        private System.Windows.Forms.Button btn_2;
-        private System.Windows.Forms.Button btn_3;
-        private System.Windows.Forms.Button btn_4;
-        private System.Windows.Forms.Button btn_5;
-        private System.Windows.Forms.Button btn_6;
-        private System.Windows.Forms.Button btn_7;
-        private System.Windows.Forms.Button btn_8;
-        private System.Windows.Forms.Button btn_9;
-        private System.Windows.Forms.Label lbl_game;
-        private System.Windows.Forms.Button btn_reset;
         private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.Button btn_AI;
+        private System.Windows.Forms.Button btn_2Player;
     }
 }
 
---
+++ noughts-and-crosses/Gameboard.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noughts_and_crosses
+{
+    class Gameboard
+    {
+        private int[] gridLayout = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        private int PlayersTurn;
+        private bool AI;
+        private int buttonNo;
+
+        public Gameboard(bool ai)
+        {
+            AI = ai;
+            PlayersTurn = 1;
+        }
+
+        public int playerMove(int playersTurn, int[] gridLayout,int buttonNo)
+        {
+            if (gridLayout[buttonNo] == 0)
+            {
+
+                 gridLayout[buttonNo] = playersTurn;
+
+                 return playersTurn;
+                 
+            }
+
+            return 0;
+            
+        }
+
+        public bool anyWinner(int[] gridLayout)
+        {
+            for (int i = 0; i < gridLayout.Length; i++)
+            {
+                if (i == 0 || i == 3 || i == 6)
+                {
+                    if (gridLayout[i] == gridLayout[i + 1] && gridLayout[i] == gridLayout[i + 2] && gridLayout[i] != 0)
+                    {
+                        return true;
+                    }
+                }
+
+                if (i == 0 || i == 1 || i == 2)
+                {
+                    if (gridLayout[i] == gridLayout[i + 3] && gridLayout[i] == gridLayout[i + 6] && gridLayout[i] != 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            if (gridLayout[0] == gridLayout[4] && gridLayout[0] == gridLayout[8] && gridLayout[0] != 0)
+            {
+                return true;
+            }
+
+            if (gridLayout[2] == gridLayout[4] && gridLayout[2] == gridLayout[6] && gridLayout[2] != 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void changePlayer()
+        {
+            if(PlayersTurn == 1)
+            {
+                PlayersTurn = 2;
+            }
+            else
+            {
+                PlayersTurn = 1;
+            }
+        }
+
+        public void reset()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                gridLayout[i] = 0;
+            }
+            PlayersTurn = 1;
+        }
+
+        public int[] getGridLayout()
+        {
+            return gridLayout;
+        }
+
+        public int getPlayersTurn()
+        {
+            return PlayersTurn; 
+        }
+    }
+}
